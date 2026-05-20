3950df087b7dcb8695710536532fde6167cda1ca
aircrafterx <mmadanraj2708@gmail.com>
2026-05-20 09:43:26 +0530

    fix: nodemailer timeout handling for production

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -9,7 +9,10 @@ const transporter = nodemailer.createTransport({
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    }
+    },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000
 })
 
 exports.register = async (req, res) => {
