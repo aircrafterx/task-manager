1fb622cb0b206e22eebdc6220b4ed0ae87f02a79
aircrafterx <mmadanraj2708@gmail.com>
2026-05-20 09:32:49 +0530

    fix: corrected register API URL

--- client/src/components/RegisterForm/index.js
+++ client/src/components/RegisterForm/index.js
@@ -27,7 +27,7 @@ class RegisterForm extends Component{
             return;
         }
         
-        const url = `${process.env.REACT_APP_API_URL}/api/auth//register`;
+        const url = `${process.env.REACT_APP_API_URL}/api/auth/register`;
         const options = {
             method: "POST",
             headers: {
